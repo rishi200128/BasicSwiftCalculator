44399f6245e4ec8bd126d22cea590cc16907da75
rishi200128 <Rishi@Rishis-MacBook-Pro.local>
2017-05-16 22:33:16 -0400

    Initial Commit

---
+++ SwiftCalculatorTests/SwiftCalculatorTests.swift
@@ -0,0 +1,36 @@
+//
+//  SwiftCalculatorTests.swift
+//  SwiftCalculatorTests
+//
+//  Created by Rishi Bhuwaneswara  on 2017-05-16.
+//  Copyright © 2017 Rishi Apps. All rights reserved.
+//
+
+import XCTest
+@testable import SwiftCalculator
+
+class SwiftCalculatorTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
